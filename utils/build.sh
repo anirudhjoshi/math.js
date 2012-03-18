a32151ef9ec6b75a8520d26fc00acef891bed8f6
Anirudh Joshi <github@anirudhjoshi.com>
2012-03-18 21:20:59 +1100

    Asseted math min

--- utils/build.sh
+++ utils/build.sh
@@ -4,8 +4,8 @@ cp ../assets/* ../build/ -r
 
 cp ../src/index.html ../build/index.html
 
-cat ../src/*.js > ../build/math.js
+cat ../src/*.js > ../build/js/math.js
 
-java -jar compiler/compiler.jar --warning_level=VERBOSE --jscomp_off=globalThis --jscomp_off=checkTypes --externs externs_common.js --language_in=ECMASCRIPT5_STRICT --js ../build/math.js --js_output_file ../build/math.min.js
+java -jar compiler/compiler.jar --warning_level=VERBOSE --jscomp_off=globalThis --jscomp_off=checkTypes --externs externs_common.js --language_in=ECMASCRIPT5_STRICT --js ../build/js/math.js --js_output_file ../build/js/math.min.js
 
-rm ../build/math.js
+rm ../build/js/math.js
