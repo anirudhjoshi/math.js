704cf3fe9a499352c0461246d25b4aca15668545
Anirudh Joshi <github@anirudhjoshi.com>
2012-03-18 21:02:20 +1100

    Overkill dep. injection

--- utils/build.sh
+++ utils/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cp ../assets/* ../build/ -r
+
+cp ../src/index.html ../build/index.html
+
 cat ../src/*.js > ../build/math.js
 
 java -jar compiler/compiler.jar --warning_level=VERBOSE --jscomp_off=globalThis --jscomp_off=checkTypes --externs externs_common.js --language_in=ECMASCRIPT5_STRICT --js ../build/math.js --js_output_file ../build/math.min.js
