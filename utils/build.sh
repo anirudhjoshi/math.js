b81cccaf91c98d780c4207544c0c1c4aaa72a152
Anirudh Joshi <github@anirudhjoshi.com>
2012-03-20 10:16:10 +1100

    Basic scaf. done - reorg'd source

--- utils/build.sh
+++ utils/build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+if [ $1 = 'all' ]; then
+  rm ../build/* -r -f
+fi
+
 cp ../assets/* ../build/ -r
 
-cp ../src/index.html ../build/index.html
+cp ../src/* ../build/
 
-cat ../src/*.js > ../build/js/math.js
+cat ../src/js/*.js > ../build/js/math.js
+cat ../src/css/*.css > ../build/css/math.css
 
 java -jar compiler/compiler.jar --warning_level=VERBOSE --jscomp_off=globalThis --jscomp_off=checkTypes --externs externs_common.js --language_in=ECMASCRIPT5_STRICT --js ../build/js/math.js --js_output_file ../build/js/math.min.js
 
