b63a4d12330345354e9998cc202121abb4781621
Anirudh Joshi <github@anirudhjoshi.com>
2012-03-23 19:57:01 +1100

    Added more functions

--- src/js/math_functions.js
+++ src/js/math_functions.js
@@ -12,3 +12,42 @@ function round( number, decimal_places ) {
          / Math.pow( 10, decimal_places );
 
 }
+
+function sinh( number ) {
+
+  // Returns the hyperbolic sine of the number, defined as (exp(number) - exp(-number))/2  
+  // original by: Onno Marsman, http://phpjs.org/functions/sinh, version: 1109.2015
+  
+  return ( Math.exp( number ) - Math.exp( -number ) ) / 2;
+
+}
+
+function asinh( number ) {
+  
+  // Returns the inverse hyperbolic sine of the number, i.e. the value whose hyperbolic sine is number
+  // original by: Onno Marsman, http://phpjs.org/functions/asinh, version: 1109.2015
+  
+  return Math.log( number + Math.sqrt( Math.pow( number, 2 ) + 1 ) );
+
+}
+
+function cosh( number ) {
+  // Returns the hyperbolic cosine of the number, defined as (exp(number) + exp(-number))/2
+  // original by: Onno Marsman, http://phpjs.org/functions/cosh, version: 1109.2015
+
+  return ( Math.exp( number ) + Math.exp( -number ) ) / 2;
+
+}
+
+
+function acosh( number ) {
+
+  // Returns the inverse hyperbolic cosine of the number, i.e. the value whose hyperbolic cosine is number  
+  // original by: Onno Marsman, http://phpjs.org/functions/acosh, version: 1109.2015
+  
+  return Math.log( number + Math.sqrt( number * number - 1 ) );
+
+}
+
+
+
